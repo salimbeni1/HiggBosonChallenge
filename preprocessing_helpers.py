d187908717f0b8c9d1a9372d954ba635d53a8fb8
salimbeni1 <etienne.salimbeni@epfl.ch>
2020-10-24 22:27:45 +0200

    powers of 2

--- preprocessing_helpers.py
+++ preprocessing_helpers.py
@@ -91,6 +91,69 @@ def select_and_expand_f( arrF):
       
     return new_arr
 
+def select_and_expand_f_pro( arrF):
+    """
+        select main features and create new ones from them
+        
+        Args:
+            arrF : (N,31) array of features , where first feature is the bias (arrF[:,0] == 1)
+                    the order of the features must be the same of the csv file.
+        
+        Returns: 
+            new_arr : (N,19) array , combination of selected and new features
+    """
+    
+    new_arr = np.zeros(( arrF.shape[0], 29 ))
+    
+    temp = 0
+    
+    # selection
+    selectInd = [ 0,  1,2,6, 12, 14, 17, 20, 23, 26]
+    for ind in range(len(selectInd)):
+        new_arr[:,ind] = arrF[:,selectInd[ind]]
+     
+    temp += len(selectInd)
+    
+    # combinations 2
+    comb2Ind = [ (14 , 22) , (8,2) , (8,3)] 
+    for ind in range(len(comb2Ind)):
+        new_arr[:,temp + ind] = arrF[:,comb2Ind[ind][0]] *arrF[:,comb2Ind[ind][1]]
+     
+    temp += len(comb2Ind)
+    
+    # combination 3
+    comb3Ind = [ (5,2,3) ]
+    for ind in range(len(comb3Ind)):
+        new_arr[:,temp + ind] = arrF[:,comb3Ind[ind][0]] *arrF[:,comb3Ind[ind][1]] *arrF[:,comb3Ind[ind][2]]
+     
+    temp += len(comb3Ind)
+    
+    # log(1**2)*5*2
+    new_arr[:,temp] = np.log(arrF[:,1]**2)*arrF[:,5] *arrF[:,2]
+    temp += 1
+    
+    # log(3**2)*3
+    new_arr[:,temp] = np.log(arrF[:,3]**2)*arrF[:,3]
+    temp += 1
+    
+    # logs(x**2)
+    logInd = [3,1,14]
+    for ind in range(len(logInd)):
+        new_arr[:,temp + ind] =np.log(arrF[:,logInd[ind]]**2)
+        
+    temp += len(logInd)
+    
+    
+      
+    # selected powers of 2
+    for ind in range(len(selectInd)):
+        new_arr[:,temp + ind] = arrF[:,selectInd[ind]]**2
+     
+    temp += len(selectInd)
+        
+    return new_arr
+
+
 
 def find_modes(x, y):
 
