da5ac82bb0cd100e786c6699c7b12d4cc0fa7f89
salimbeni1 <etienne.salimbeni@epfl.ch>
2020-10-26 11:07:12 +0100

    run.py update

--- run.py
+++ run.py
@@ -5,12 +5,14 @@ from Basics import *
 from saved_weight import w_after_20000_steps
 
 
+print("Project 1")
+
 DATA_TRAIN_PATH = 'data/train.csv' 
 y, tX, ids = load_csv_data(DATA_TRAIN_PATH)
 DATA_TEST_PATH = 'data/test.csv'
 y_test, tX_test, ids_test = load_csv_data(DATA_TEST_PATH)
 
-print("train and test csv parsed")
+print("done : train and test csv parsing")
 
 trainY , trainF = y, tX
 testY , testF = y_test, tX_test
@@ -25,30 +27,35 @@ testF = np.insert(testF ,0 , np.ones(testF.shape[0]),axis = 1) # add bias
 # replace -999 with mode
 trainF, testF = replace_missing_values(trainF,testF, 'mode')
 
+print("done : replacing missing values with mode")
+
 # feature selection expansion
 trainF_sel = select_and_expand_f_logistic(trainF)
 testF_sel = select_and_expand_f_logistic(testF)
 
-print("preprocessing done")
+print("done : feature expansion (",trainF_sel.shape[1]-1,"features )")
 
 #### TRAINING ######################################################
 #w_inital = np.random.rand(trainF_sel.shape[1])
 
-# commented because this training can last more than 30min
+# commented because this training can last more than 40min
 # you can make less steps but the accuracy wont be optimal
 # w, _ = logistic_regression(trainY, trainF_sel, w_inital , 20000 ,0.07/y.shape[0])
 
 # use pretrained array
 w = np.array(w_after_20000_steps)
 
+print("done : weights after trainning")
 
 #### prediction ###################################################
 
 y_pred = predict_labels(w  , testF_sel)
 
 
-print("prediction done")
+print("done : computing predict lables")
 
 # save prediction
 OUTPUT_PATH = 'output_sub1000.csv'
 create_csv_submission(ids_test, y_pred, OUTPUT_PATH)
+
+print("done : saving prediction on ",OUTPUT_PATH)
